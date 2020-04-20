bc7ab31dc1aa36f6435a6d44debc6b6f6bd3f424
Josh Yap <joshua.c.yap@gmail.com>
2020-04-19 23:14:02 -0500

    testing netlify

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ export default () => (
     {/* <Link to="/contact/">Contact</Link> */}
     {/* <Header headerText="Hi, I'm Josh" /> */}
     <h1>IN PROGRESS</h1>
-    <h3>I am in the process of experimenting with a full JAMstack site and testing deployment solutions. This is not a finished product by any means.</h3>
+    <h3>I am in the process of rebuilding my portfolio/blog site with react as a JAMstack site. This is not a finished product by any means.</h3>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
   </Layout>
 )
