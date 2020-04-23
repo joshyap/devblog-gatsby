78ed2bf115ab45bd76b46c86a2a58a261f21936e
Josh Yap <joshua.c.yap@gmail.com>
2020-04-22 23:50:46 -0500

    contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,18 @@
 import React from "react"
 import Layout from "../components/layout"
+import contactStyles from "./contact.module.css"
 
 export default () => (
   <Layout>
     {/* <Header headerText="Contact" /> */}
-    <p>Send us a message!</p>
+    <h3>Looking for me?</h3>
+    <ul>
+      <li className={contactStyles.contactList}><a href="https://www.linkedin.com/in/joshuacyap/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+      <li className={contactStyles.contactList}><a href="https://github.com/joshyap" target="_blank" rel="noopener noreferrer">Github</a></li>
+      <li className={contactStyles.contactList}><a href="https://twitter.com/J0SHYAP" target="_blank" rel="noopener noreferrer">Twitter</a></li>
+      <li className={contactStyles.contactList}><a href="https://dev.to/j0shyap" target="_blank" rel="noopener noreferrer">dev.to</a></li>
+      <li className={contactStyles.contactList}><a href="https://www.freecodecamp.org/joshyap" target="_blank" rel="noopener noreferrer">freeCodeCamp</a></li>
+      <li className={contactStyles.contactList}><a href="https://www.chess.com/member/j0shyap" target="_blank" rel="noopener noreferrer">Chess.com</a></li>
+    </ul>
   </Layout>
 )
