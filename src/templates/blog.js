2f57393fdb55fc3d5a76aa1aed736cd446e80dcd
Josh Yap <joshua.c.yap@gmail.com>
2020-04-28 23:20:28 -0500

    blog templating!

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,12 +1,67 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { graphql } from 'gatsby'
+// import { useStaticQuery } from '../../node_modules/gatsby';
+// import { extendingDifferentTypeKindMessage } from '../../node_modules/graphql/validation/rules/PossibleTypeExtensions';
 
-const Blog = () => {
+export const query = graphql`
+    query($slug: String!) {
+        markdownRemark(fields: { slug: { eq : $slug } }) {
+            frontmatter {
+                title
+                date
+            }
+            html
+        }
+    }
+`
+
+const Blog = (props) => {
     return (
         <Layout>
-            This is blog templates
+            <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+            <p>{props.data.markdownRemark.frontmatter.date}</p>
+            <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}></div>
         </Layout>
     )
 }
+
+// const Blog = () => {
+//     const data = useStaticQuery(graphql`
+//         query {
+//             allMarkdownRemark {
+//                 edges {
+//                     node {
+//                         frontmatter {
+//                             title
+//                             date
+//                         }
+//                         fields {
+//                             slug
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     `)
+    
+//     return (
+//         <Layout>
+//             <h1>Blog</h1>
+//             <ol>
+//                 {data.allMarkdownRemark.edges.map((edge) => {
+//                     return (
+//                         <li>
+//                             <Link to={`/blog/$edge.node.fields.slug}`}>
+//                                 <h2>{edge.node.frontmatter.title}</h2>
+//                                 <p>{edge.node.frontmatter.date}</p>
+//                             </Link>
+//                         </li>
+//                     )
+//                 })}
+//             </ol>            
+//         </Layout>
+//     )
+// }
 
 export default Blog
