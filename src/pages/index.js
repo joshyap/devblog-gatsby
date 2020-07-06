a8c6a2d9986ca043328b2ce0d8b5c0d70883361b
Josh Yap <joshua.c.yap@gmail.com>
2020-07-05 21:33:21 -0500

    homepage redo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 // import Layout from "../components/layout"
-<<<<<<< HEAD
 import indexStyles from "./index.module.css"
 import Header from "../components/header"
 import Footer from "../components/footer"
@@ -35,29 +34,3 @@ export default () => (
     // <h3>I am in the process of rebuilding my portfolio/blog site with react as a JAMstack site. This is not a finished product by any means.</h3>
     // {/* <img src="https://source.unsplash.com/random/400x200" alt="" /> */}
     // {/* <img src={'/marfa.jpg'} alt="Prada store in Marfa" className={indexStyles.homepageImage} /> */}
-=======
-import Hero from "../components/hero"
-// import indexStyles from "./index.module.css"
-
-export default () => (
-  <Hero>
-    <h1>IN PROGRESS</h1>
-    <h3>I am in the process of rebuilding my portfolio/blog site with react as a JAMstack site. This is not a finished product by any means.</h3>
-  </Hero>    
-)
-
-
-// export default () => (
-//   <Layout>
-//     {/* <Link to="/contact/">Contact</Link> */}
-//     {/* <Header headerText="Hi, I'm Josh" /> */}
-//     <Hero>
-//       <h1>IN PROGRESS</h1>
-//       <h3>I am in the process of rebuilding my portfolio/blog site with react as a JAMstack site. This is not a finished product by any means.</h3>
-//     </Hero>    
-//     {/* <img src="https://source.unsplash.com/random/400x200" alt="" /> */}
-//     <img src={'/marfa.jpg'} alt="Prada store in Marfa" className={indexStyles.homepageImage} />
-//   </Layout>
-// )
-
->>>>>>> b81df51504d3a32cef4081edd06cd7678617af1d
